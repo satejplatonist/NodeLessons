216f308818e4667fdcc03a0bf0bc9971571316cd
satejplatonist <satejshambho@gmail.com>
2025-07-20 19:28:34 +0530

    node js first lesson

---
+++ hello-world-node/index.js
@@ -0,0 +1,10 @@
+console.log("hello node js");
+array = [1,2,"3"];
+
+console.log("this is array : ", array);
+
+setTimeout(()=>{
+   console.log("This message is delayed by 2 seconds")
+},2000);
+
+console.log("this is last line of sync code");
