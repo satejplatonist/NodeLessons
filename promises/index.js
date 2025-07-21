2ec65e21cd6191c12d02d38346abf35bd8ba7ee1
satejplatonist <satejshambho@gmail.com>
2025-07-21 14:26:01 +0530

    learned promises - object representing eventual completion/failure of asynchrnous task and its resulting value

---
+++ promises/index.js
@@ -0,0 +1,22 @@
+function delay(time) 
+{
+    return new Promise((resolve,reject) => {
+        if(time==0)
+        {
+            reject("Task will take time ...");
+        }
+        else
+        {
+            resolve(setTimeout(()=>{
+                console.log(`after ${time/1000} seconds promise resolved ....`);
+            },time))
+        }
+    })    
+}
+
+console.log("Promise lecture : ");
+delay(8000).then(()=>{console.log("after 2 seconds promise resolved : ");})
+           .catch((error)=>{console.log("Error is : ",error);})
+           .finally(()=>{console.log("Promise settled .....")});
+
+console.log("end")
