ec5681c5ba11dad0236bc9fe6aa4c3be350100ed
satejplatonist <satejshambho@gmail.com>
2025-07-25 23:09:18 +0530

    did front end setup

---
+++ auth-full/frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-4xl bg-gradient-to-r bg-clip-text text-transparent from-cyan-600 to-cyan-400'>React - tailwind - vite setup done</h1>
+    </>
+  )
+}
+
+export default App
